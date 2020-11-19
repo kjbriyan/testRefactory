4e87ab39950e68e42ed39618d45e056fc1d70e53
kjbriyan <kjbriyan@gmail.com>
2020-11-19 20:42:56 +0700

    finish

--- app/src/main/java/com/arsitek/test/MainActivity.kt
+++ app/src/main/java/com/arsitek/test/MainActivity.kt
@@ -2,27 +2,37 @@ package com.arsitek.test
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.arsitek.test.TextUndoRedo.TextChangeInfo
 
+// tidak merekomendasikan doble click digunakan untuk action ini
+// dikarenakan Menerapkan Sentuhan Ganda akan memengaruhi penanganan Sentuhan Tunggal, karena Anda
+// harus menunggu untuk melihat apakah setiap Sentuhan Tunggal berubah menjadi Sentuhan Ganda sebelum Anda
+// dapat memprosesnya.
 
-class MainActivity : Activity(), TextChangeInfo, View.OnClickListener {
+class MainActivity : Activity(), TextChangeInfo {
     private var TUR: TextUndoRedo? = null
     private var btn_undo: Button? = null
     private var btn_redo: Button? = null
+    private  var btn_revert : Button? = null
+    private var tv : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
-        btn_undo = findViewById<View>(R.id.btn_undo) as Button
-        btn_undo!!.setOnClickListener(this)
-        btn_redo = findViewById<View>(R.id.btn_redo) as Button
-        btn_redo!!.setOnClickListener(this)
-        findViewById<View>(R.id.btn_clean).setOnClickListener(this)
+        setContentView(R.layout.activity_main)
+        btn_undo = findViewById(R.id.btn_undo)
+        btn_redo = findViewById(R.id.btn_redo)
+        btn_revert= findViewById(R.id.btn_revert)
+        tv = findViewById(R.id.textView)
         TUR = TextUndoRedo(findViewById<View>(R.id.et_area) as EditText, this)
         textAction()
+        bot()
     }
 
     override fun textAction() {
@@ -30,42 +40,42 @@ class MainActivity : Activity(), TextChangeInfo, View.OnClickListener {
         btn_redo!!.isEnabled = TUR!!.canRedo()
     }
 
-    override fun onClick(view: View) {
-        when (view.id) {
-            R.id.btn_undo -> btn()
-            R.id.btn_redo -> TUR!!.exeRedo()
-            R.id.btn_clean -> {
-                TUR!!.cleanRecord()
-                textAction()
-            }
+    fun bot(){
+        btn_redo?.setOnClickListener {
+            TUR!!.exeRedo()
         }
-    }
+        btn_revert?.setOnClickListener {
+            var txt = findViewById<EditText>(R.id.et_area).text
+            var get = reverse(txt.toString())
+            tv?.setText(get)
+        }
 
-    fun btn (){
-        btn_undo?.setOnClickListener {
-            Toast.makeText(this@MainActivity,"singgle",Toast.LENGTH_SHORT).show()
-        }
-        btn_undo?.setOnClickListener(object : DoubleClickListener() {
-            override fun onDoubleClick(v: View?) {
-                Toast.makeText(this@MainActivity,"Double Click",Toast.LENGTH_SHORT).show()
+        btn_undo?.setOnTouchListener(object : View.OnTouchListener{
+            val gestureDetector = GestureDetector(object : GestureDetector.SimpleOnGestureListener(){
+                override fun onSingleTapUp(e: MotionEvent?): Boolean {
+                    TUR!!.exeUndo()
+                    Toast.makeText(this@MainActivity, "Singgle Click", Toast.LENGTH_SHORT).show()
+                    return super.onSingleTapUp(e)
+                }
+                override fun onDoubleTap(e: MotionEvent?): Boolean {
+                    TUR!!.exeRedo()
+                    Toast.makeText(this@MainActivity, "Double Click", Toast.LENGTH_SHORT).show()
+                    return super.onDoubleTap(e)
+                }
+            })
+
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                //do something here
+                gestureDetector.onTouchEvent(event)
+                return true
             }
         })
     }
 
-    abstract class DoubleClickListener : View.OnClickListener {
-        var lastClickTime: Long = 0
-        override fun onClick(v: View?) {
-            val clickTime = System.currentTimeMillis()
-            if (clickTime - lastClickTime < DOUBLE_CLICK_TIME_DELTA) {
-                onDoubleClick(v)
-            }
-            lastClickTime = clickTime
-        }
+    fun reverse(sentence: String): String {
+        if (sentence.isEmpty())
+            return sentence
 
-        abstract fun onDoubleClick(v: View?)
-
-        companion object {
-            private const val DOUBLE_CLICK_TIME_DELTA: Long = 300 //milliseconds
-        }
+        return reverse(sentence.substring(1)) + sentence[0]
     }
 }
